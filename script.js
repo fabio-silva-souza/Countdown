22971923bba21d3671e1c3081c1248772c36be92
fabio-silva-souza <silvafabiosouza@hotmail.com>
2022-12-26 17:09:47 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const daysEl  = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minEl   = document.getElementById('min');
+const seconEl = document.getElementById('secon');
+
+const newYears = '1 Jan 2023';
+
+function countdown () {
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+
+    const totalSecond = (newYearsDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSecond / 3600 / 24);
+    const hours = Math.floor(totalSecond / 3600) % 24;
+    const minutes = Math.floor(totalSecond / 60) % 60;
+    const seconds = Math.floor(totalSecond) % 60
+    
+    daysEl.innerHTML = days
+    hoursEl.innerHTML = formatTime(hours)
+    minEl.innerHTML = formatTime(minutes)
+    seconEl.innerHTML = formatTime(seconds)
+}
+
+function formatTime(time) {
+    return time < 10 ? ( `0${time}`) : time;
+}
+countdown();
+
+setInterval(countdown,1000)
